e30042715c58cd3cee9c8797cb37116de2d5256e
Manpreet Tiwari <manpreet.mobicloud@gmail.com>
2023-02-20 14:17:28 +0530

    initial commit

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
+    console.log(`Server is running on port http://localhost:${PORT}/`)
 })
